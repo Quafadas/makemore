6e096d5f9317fd58be375ae1ce12b0373138975f
Simon Parten <Quafadas@users.noreply.github.com>
2025-07-16 16:07:17 +0000

    .

--- project.scala
+++ project.scala
@@ -4,7 +4,7 @@
 
 //> using dep io.github.quafadas::scautable:0.0.24
 //> using dep io.github.quafadas::vecxt::0.0.31
-//> using dep io.github.quafadas::spireAD::0.0.10-20-50727a
+//> using dep io.github.quafadas::inspireRAD::0.0.15
 //> using dep io.github.quafadas::dedav4s:0.9.3
 //> using dep org.apache.commons:commons-math3:3.6.1
 //> using dep org.typelevel::spire::0.18.0
