f68181aa08c394026292669ff6bcc193b9c4dd36
Simon Parten <quafadas@gmail.com>
2025-10-29 12:24:35 +0100

    .

--- makemore_random.scala
+++ makemore_random.scala
@@ -3,22 +3,22 @@ import viz.PlotTargets.desktopBrowser
 import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution
 import scala.io.Source
 /**
- * Strategy:
+ * Live sketch:
   1. Generate chars, chars index and a bimap
-  2. Combinations of all bigrams
-  3. Assume a unifiorm distribution
-  4. Count pairs
-  5. plot heatmap
-  6. group by first char. Then Map the complete group into a EnumeratedIntegerDistribution
- *
+  3. val normalisd = [Assume a unifiorm distribution]
+  4. heatmap( [PLOT]
+  5. def generator( [GENERATE NAMES]
+  6. println("Rubbish name generator") [Generate names]
+  7. val someNames = Seq(...) [NAMES TO CHECK]
+  8. checkWords(someNames, normalised, charsMap).ptbln
  */
+// scala-cli run . --main-class makemore_random_live -w
 @main def makemore_random_live: Unit =
 
   val chars = '.' +: ('a' to 'z').toVector
   val charsIndex = (0 to 26).toVector
   val charsMap = chars.zip(charsIndex).toMap
   val i2c = charsIndex.zip(chars).toMap
-
 
   println("Characters: " + chars)
   println("Character to Index map: " + charsMap)
